ef0f126ea9b093e77a737acda9ef05dbe93dfb5b
Aaron Smith <smitham50@gmail.com>
2019-11-18 12:26:30 -0500

    mostly works?

--- index.js
+++ index.js
@@ -31,23 +31,61 @@ clear.addEventListener('click', () => {
 //operators
 const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equal-sign');
-let num1, num2;
+let num1, num2, adding = false, subtracting = false, dividing = false, multiplying = false;
 
 operators.forEach(operator => {
   operator.addEventListener('click', (e) => {
     afterOperator = true;
+    num1 = parseInt(disp.value);
     //add
     if (e.target.value === '+') {
-      num1 = parseInt(disp.value);
-
-      //after equals
-      equals.addEventListener('click', () => {
-        num2 = parseInt(disp.value);
-        disp.value = (num1 + num2).toString();
-        num1 = 0;
-        num2 = 0;
-      })
+      adding = true;
     }
+      
+
+    //subtract
+    if (e.target.value === '-') {
+      subtracting = true;
+    }
+
+    //divide
+    if (e.target.value === '/') {
+      dividing = true;
+    }
+
+    //multiply
+    if (e.target.value === '*') {
+      multiplying = true;
+    }
+
+    //after equals
+    equals.addEventListener('click', () => {
+      num2 = parseInt(disp.value);
+      
+      if (adding) {
+        disp.value = (num1 + num2).toString();
+        adding = false;
+      };
+
+      if (subtracting) {
+        disp.value = (num1 - num2).toString();
+        subtracting = false;     
+      };
+
+      if (multiplying) {
+        disp.value = (num1 * num2).toString();
+        multiplying = false; 
+      };
+
+      if (dividing) {
+        disp.value = (num1 / num2).toString();
+        dividing = false;
+      }
+
+      num1 = 0;
+      num2 = 0;
+    })
+ 
   });
 });
 
