8dc01b722c5f208f22f8f1ac7e1b7cf1d6e81a59
Aaron Smith <smitham50@gmail.com>
2019-11-18 11:10:39 -0500

    started operator functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ ints.forEach(int => {
     if (disp.value.length < 12) {
       disp.value === "0" ? disp.value = e.target.value : disp.value += e.target.value;
     }
-  })
+  });
 });
 
 //clear display
@@ -18,5 +18,28 @@ const clear = document.querySelector('.all-clear');
 
 clear.addEventListener('click', () => {
   disp.value = "0";
-})
+});
+
+//operators
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equal-sign');
+
+operators.forEach(operator => {
+  operator.addEventListener('click', (e) => {
+    //add
+    if (e.target.value === '+') {
+      let num1 = parseInt(disp.value);
+      let num2;
+      let firstClick = false;
+      //enter next num and clear display
+      
+       
+      num2 = parseInt(disp.value);
+      equals.addEventListener('click', (num1, num2) => {
+        disp.value = (num1 + num2).toString();
+      })
+    }
+  });
+});
+
 
